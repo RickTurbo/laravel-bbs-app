555b640ddba0015fb9b6496a9df4de4e13bd3bbe
kojima rikuto <kojikoji7765@icloud.com>
2022-01-31 22:04:55 +0900

    ロゴやメニューの作成

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,15 +5,20 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('threads') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
+                    <div class="w-12">
+                        <a href="{{ route('threads') }}">
+                            <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        </a>
+                    </div>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('threads')" :active="request()->routeIs('threads')">
+                        掲示板
                     </x-nav-link>
                 </div>
             </div>
